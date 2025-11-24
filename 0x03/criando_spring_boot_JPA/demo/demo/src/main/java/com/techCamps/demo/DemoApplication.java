4dcd4350819736ff94e501c5421c3cc98642b7cc
leandro Ribeiro de Lima <leandro.r.lima@next.b.br>
2025-11-24 14:05:57 -0300

    microservico jpa

--- 0x03/criando_spring_boot_JPA/demo/demo/src/main/java/com/techCamps/demo/DemoApplication.java
+++ 0x03/criando_spring_boot_JPA/demo/demo/src/main/java/com/techCamps/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.techCamps.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SongApiApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SongApiApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
